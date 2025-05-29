7e6cbbbc21276f1730019473ac36f9f7f7496096
Renat <renat.ochakaevbw@mail.ru>
2025-05-30 01:07:37 +0300

    edit

--- src/World/world.cpp
+++ src/World/world.cpp
@@ -26,9 +26,11 @@ void World::render() const {
     //     entity->render();
     // }
 
-    // for (const auto & tile : _tiles) {
-    //     tile.render(_tileset);
-    // }
+    for (const auto & tile : _tiles) {
+        tile.render(_tileset);
+    }
+
+    //DrawTexture(_tileset, 0, 0, WHITE);
 
 }
 
@@ -40,7 +42,101 @@ void World::update() {
 
 }
 
+#include <tmxlite/Map.hpp>
+#include <tmxlite/TileLayer.hpp>
+
 bool World::loadFromFile(const std::string& filename) {
+    // Полный путь к файлу
+    std::string fullPath = "resources/worlds/" + filename;
+    
+    // 1. Загрузка карты
+    tmx::Map map;
+    if (!map.load(fullPath)) {
+        TraceLog(LOG_ERROR, "Failed to load map: %s", fullPath.c_str());
+        return false;
+    }
+    
+    // 2. Получение tileset'а (первого в списке)
+    const auto& tilesets = map.getTilesets();
+    if (tilesets.empty()) {
+        TraceLog(LOG_ERROR, "No tilesets found in map");
+        return false;
+    }
+    
+    const auto& tileset = tilesets[0];
+    std::string tilesetPath = tileset.getImagePath();
+    
+    // 3. Загрузка текстуры tileset'а
+    _tileset = LoadTexture(tilesetPath.c_str());
+    if (_tileset.id == 0) {
+        TraceLog(LOG_ERROR, "Failed to load tileset texture: %s", tilesetPath.c_str());
+        return false;
+    }
+    
+    TraceLog(LOG_INFO, "Successfully loaded tileset: %s", tilesetPath.c_str());
+    
+    
+    const auto& layers = map.getLayers();
+    for (const auto& layer : layers) {
+        if (layer->getType() == tmx::Layer::Type::Tile) {
+            const auto& tileLayer = layer->getLayerAs<tmx::TileLayer>();
+            
+            // Сохраняем размеры карты
+            _width = tileLayer.getSize().x;
+            _height = tileLayer.getSize().y;
+            
+            // Заполняем сетку
+            const auto& tiles = tileLayer.getTiles();
+            _grid.reserve(tiles.size());
+            
+            for (const auto& tile : tiles) {
+                _grid.push_back(tile.ID); // Сохраняем ID тайлов
+            }
+            
+            TraceLog(LOG_INFO, "Loaded tile layer with %dx%d tiles", _width, _height);
+            break; // Пока берем только первый слой
+        }
+    }
+
+        // 5. Создание объектов Tile
+        const auto& tilesetInfo = map.getTilesets()[0];
+        unsigned int tileWidth = tilesetInfo.getTileSize().x;
+        unsigned int tileHeight = tilesetInfo.getTileSize().y;
+        
+        for (int y = 0; y < _height; ++y) {
+            for (int x = 0; x < _width; ++x) {
+                int index = y * _width + x;
+                unsigned int tileID = _grid[index];
+                
+                if (tileID == 0) continue; // 0 - пустой тайл
+                
+                // Позиция на карте
+                Rectangle destRec = {
+                    x * tileWidth * 1.0f,
+                    y * tileHeight * 1.0f,
+                    tileWidth * 1.0f,
+                    tileHeight * 1.0f
+                };
+                
+                // Позиция в tileset'е
+                unsigned int tilesetColumns = tilesetInfo.getColumnCount();
+                unsigned int tilesetX = (tileID - 1) % tilesetColumns;
+                unsigned int tilesetY = (tileID - 1) / tilesetColumns;
+                
+                Rectangle sourceRec = {
+                    tilesetX * tileWidth * 1.0f,
+                    tilesetY * tileHeight * 1.0f,
+                    tileWidth * 1.0f,
+                    tileHeight * 1.0f
+                };
+                
+                _tiles.emplace_back(sourceRec, destRec);
+            }
+        }
+        
+        TraceLog(LOG_INFO, "Created %d tile objects", _tiles.size());
+
+
     return true;
 }
 
